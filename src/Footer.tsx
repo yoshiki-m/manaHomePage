e0d222f225786fd8158ad3da3b5098c72ab775d2
yoshiki-m <m.yoshiki0124@gmail.com>
2021-07-07 09:13:32 +0900

    2021.7.7

---
+++ src/Footer.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { VFC } from 'react';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+const Copyright: VFC = () => (
+  <Typography variant="body2" color="text.secondary" align="center">
+    {'Copyright © '}
+    <Link color="inherit" href="https://material-ui.com/">
+      Your Website
+    </Link>{' '}
+    {new Date().getFullYear()}.
+  </Typography>
+);
+
+interface FooterProps {
+  description: string;
+  title: string;
+}
+
+const Footer: VFC<FooterProps> = (props) => {
+  const { description, title } = props;
+
+  return (
+    <Box component="footer" sx={{ bgcolor: 'background.paper', py: 6 }}>
+      <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom>
+          {title}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          {description}
+        </Typography>
+        <Copyright />
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
