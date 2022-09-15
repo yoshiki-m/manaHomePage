289305958d0f4a55029ee33dc1a9aed9a8a5a4a4
yoshiki-m <m.yoshiki0124@gmail.com>
2022-09-15 20:50:18 +0900

    Updates

---
+++ precache-manifest.7a7e5937f8736ddbd21b20dc8c64b6a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d963285c8b3004acbc97",
+    "url": "/manaHomePage/static/css/main.5328a8b3.chunk.css"
+  },
+  {
+    "revision": "d963285c8b3004acbc97",
+    "url": "/manaHomePage/static/js/main.d963285c.chunk.js"
+  },
+  {
+    "revision": "5dcfbbf9e415a00e9656",
+    "url": "/manaHomePage/static/js/1.5dcfbbf9.chunk.js"
+  },
+  {
+    "revision": "581d0e015490455941ed",
+    "url": "/manaHomePage/static/js/2.581d0e01.chunk.js"
+  },
+  {
+    "revision": "0b40ef2851d61233fdbb",
+    "url": "/manaHomePage/static/js/runtime~main.0b40ef28.js"
+  },
+  {
+    "revision": "98f6e30bce13257c0cc5b4b9cea700b8",
+    "url": "/manaHomePage/static/media/top.98f6e30b.png"
+  },
+  {
+    "revision": "6b674f5ea76a5a93be55cd100bc95e31",
+    "url": "/manaHomePage/static/media/AIPlanner_1.6b674f5e.png"
+  },
+  {
+    "revision": "8e04cc10b745b7040900a6b48538a9d9",
+    "url": "/manaHomePage/static/media/AIPlanner_2.8e04cc10.png"
+  },
+  {
+    "revision": "f406b593142179eaea5899b069f56b9d",
+    "url": "/manaHomePage/static/media/AIPlanner_3.f406b593.png"
+  },
+  {
+    "revision": "0591b6d74fc62f79b3a608cce4039f4c",
+    "url": "/manaHomePage/index.html"
+  }
+];
